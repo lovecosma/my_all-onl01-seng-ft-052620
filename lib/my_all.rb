5160b9d57f7c8d73db9869c68e2b5fafdfc946bf
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-29 17:55:22 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
-
+if collection.length > 0
+i = 0
+new_collection = []
+while i < collection.length
+  new_collection << yield(collection[i])
+  i+=1
 end
+if new_collection.include?(false)
+  return false
+else
+  return true
+end
+else
+  puts "nothing in array"
+end
+end 
